a3163518f89e96e3248773f12db2db451fb40b1c
chenyonghuazi <edwinchenyonghua@gmail.com>
2018-02-17 09:23:14 -0500

    Initial Commit

---
+++ UTMB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UTMB
+//
+//  Created by Edwin on 2018/2/17.
+//  Copyright © 2018年 Edwin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
